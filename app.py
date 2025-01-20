ab4f5ba668f4e11f7eafcebc1056eca240e504fa
mossy1345 <mossyboibro123456@gmail.com>
2025-01-19 22:54:46 -0500

    First Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+
+import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session, send_from_directory, get_flashed_messages
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
+import datetime
+
+app = Flask(__name__)
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return render_template('index.html')
